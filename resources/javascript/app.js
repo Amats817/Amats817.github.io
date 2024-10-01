a955a5da0d2cfaeba028c66eac385a4ce511e57f
Mark Joshua Sto Domingo <mjgstodomingo@gmail.com>
2024-09-30 22:45:31 -0500

    improved mobile compatibility

--- resources/javascript/app.js
+++ resources/javascript/app.js
@@ -7,3 +7,14 @@ function contactScroll(){
         behavior: 'smooth'
     });
 };
+
+//header for mobile
+let headerContainer = document.querySelector('.header-container');
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 100) {
+        headerContainer.classList.add('scrolled');
+    } else {
+        headerContainer.classList.remove('scrolled');
+    }
+});
