4b6d125d98aa92c32a973d5e8625840a1102696e
Mark Joshua Sto Domingo <mjgstodomingo@gmail.com>
2024-10-01 08:49:49 -0500

    fixed header, added better icons

--- resources/javascript/app.js
+++ resources/javascript/app.js
@@ -7,14 +7,3 @@ function contactScroll(){
         behavior: 'smooth'
     });
 };
-
-//header for mobile
-let headerContainer = document.querySelector('.header-container');
-
-window.addEventListener('scroll', () => {
-    if (window.scrollY > 100) {
-        headerContainer.classList.add('scrolled');
-    } else {
-        headerContainer.classList.remove('scrolled');
-    }
-});
